81965683150dc7982f1d6f442a82d3cf9393bde7
shahin <shahinahd3@gamil.com>
2024-03-12 14:28:58 +0530

    Basic Component HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import { Button, Container, Stack } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Container className='my-4'>
+      <Stack direction='horizontal' gap="2" className='mb-4'>
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button> 
+      </Stack>
+      <div style={{
+        display:"grid",
+        gridTemplateColumns:"repeat(auto-fill,minmax(300px,1fr))",
+        gap:"1rem",
+        alignItems:"flex-start"
+      }}></div>
+    </Container>
   );
 }
 
