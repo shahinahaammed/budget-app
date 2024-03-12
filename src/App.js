6d6542290164b11c145a470f1ce1b7bdb8001f60
shahin <shahinahd3@gamil.com>
2024-03-12 15:33:09 +0530

    Budget Card Component HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { Button, Container, Stack } from 'react-bootstrap';
-import './App.css';
+import { Button, Container, Stack } from "react-bootstrap";
+import "./App.css";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
-    <Container className='my-4'>
-      <Stack direction='horizontal' gap="2" className='mb-4'>
-        <h1 className='me-auto'>Budgets</h1>
-        <Button variant='primary'>Add Budget</Button>
-        <Button variant='outline-primary'>Add Expense</Button> 
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
       </Stack>
-      <div style={{
-        display:"grid",
-        gridTemplateColumns:"repeat(auto-fill,minmax(300px,1fr))",
-        gap:"1rem",
-        alignItems:"flex-start"
-      }}></div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard
+          name="Entertainment"
+          gray
+          amount={109999999999200}
+          max={10000}
+        ></BudgetCard>
+      </div>
     </Container>
   );
 }
